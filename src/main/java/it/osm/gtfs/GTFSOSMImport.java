b5ac6f540edf4144c5f4bc6d30d50fc58eaeec3c
Gabriele <gabboxl0@gmail.com>
2022-12-03 19:37:33 +0100

    Added ability to start the program in interactive mode
    useful for in IDE testing of the commands

--- src/main/java/it/osm/gtfs/GTFSOSMImport.java
+++ src/main/java/it/osm/gtfs/GTFSOSMImport.java
@@ -18,16 +18,32 @@ package it.osm.gtfs;
 import it.osm.gtfs.command.*;
 import it.osm.gtfs.command.gui.GTFSRouteDiffGui;
 import it.osm.gtfs.utils.GTFSImportSetting;
-import org.json.JSONException;
+import org.jline.console.SystemRegistry;
+import org.jline.console.impl.Builtins;
+import org.jline.console.impl.SystemRegistryImpl;
+import org.jline.keymap.KeyMap;
+import org.jline.reader.*;
+import org.jline.reader.impl.DefaultParser;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import org.jline.widget.TailTipWidgets;
 import org.xml.sax.SAXException;
 import picocli.CommandLine;
+import picocli.shell.jline3.PicocliCommands;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Supplier;
 
-@CommandLine.Command(name = "GTFSOSMImport", subcommands = {
+
+
+
+@CommandLine.Command(name = "GTFSOSMImport", /* capire a cosa serve bene sta variabile */mixinStandardHelpOptions = true, subcommands = {
 		GTFSUpdateDataFromOSM.class, GTFSGenerateBusStopsImport.class,
 GTFSGetBoundingBox.class, GTFSGenerateRoutesGPXs.class, GTFSGenerateRoutesBaseRelations.class, GTFSGenerateRoutesFullRelations.class,
 GTFSMatchGPX.class, GTFSCheckOsmRoutes.class,
@@ -76,12 +92,116 @@ public class GTFSOSMImport {
 				"Plugin Class: " + GTFSImportSetting.getInstance().getPlugin().getClass().getCanonicalName() + "\n");
 	}
 
+
+	@CommandLine.Command(description = {
+					"Example interactive shell with completion and autosuggestions. " +
+							"Hit @|magenta <TAB>|@ to see available commands.",
+					"Hit @|magenta ALT-S|@ to toggle tailtips.",
+					""},
+			footer = {"", "Press Ctrl-D to exit."},
+			subcommands = {
+					GTFSUpdateDataFromOSM.class, GTFSGenerateBusStopsImport.class,
+					GTFSGetBoundingBox.class, GTFSGenerateRoutesGPXs.class, GTFSGenerateRoutesBaseRelations.class, GTFSGenerateRoutesFullRelations.class,
+					GTFSMatchGPX.class, GTFSCheckOsmRoutes.class,
+					GTFSGenerateRoutesDiff.class, PicocliCommands.ClearScreen.class, CommandLine.HelpCommand.class})
+	static class CliCommands implements Runnable {
+		PrintWriter out;
+
+		CliCommands() {}
+
+		public void setReader(LineReader reader){
+			out = reader.getTerminal().writer();
+		}
+
+		public void run() {
+			out.println(new CommandLine(this).getUsageMessage());
+		}
+	}
+
+
+
+	//@CommandLine.Command(description = "Start GTFS-OSM-import in shell interactive mode")
+	static void interactive(){
+		try {
+			Supplier<Path> workDir = () -> Paths.get(System.getProperty("user.dir"));
+			// set up JLine built-in commands
+			Builtins builtins = new Builtins(workDir, null, null);
+			builtins.rename(Builtins.Command.TTOP, "top");
+			builtins.alias("zle", "widget");
+			builtins.alias("bindkey", "keymap");
+			// set up picocli commands
+			CliCommands commands = new CliCommands();
+
+			PicocliCommands.PicocliCommandsFactory factory = new PicocliCommands.PicocliCommandsFactory();
+			// Or, if you have your own factory, you can chain them like this:
+			// MyCustomFactory customFactory = createCustomFactory(); // your application custom factory
+			// PicocliCommandsFactory factory = new PicocliCommandsFactory(customFactory); // chain the factories
+
+			CommandLine cmd = new CommandLine(new GTFSOSMImport(), factory); //CommandLine cmd = new CommandLine(commands, factory); vecchia stringahel
+			PicocliCommands picocliCommands = new PicocliCommands(cmd);
+
+			Parser parser = new DefaultParser();
+			try (Terminal terminal = TerminalBuilder.builder().build()) {
+				SystemRegistry systemRegistry = new SystemRegistryImpl(parser, terminal, workDir, null);
+				systemRegistry.setCommandRegistries(builtins, picocliCommands);
+				systemRegistry.register("help", picocliCommands);
+
+				LineReader reader = LineReaderBuilder.builder()
+						.terminal(terminal)
+						.completer(systemRegistry.completer())
+						.parser(parser)
+						.variable(LineReader.LIST_MAX, 50)   // max tab completion candidates
+						.build();
+				builtins.setLineReader(reader);
+				commands.setReader(reader);
+				factory.setTerminal(terminal);
+				TailTipWidgets widgets = new TailTipWidgets(reader, systemRegistry::commandDescription, 5, TailTipWidgets.TipType.COMPLETER);
+				widgets.enable();
+				KeyMap<Binding> keyMap = reader.getKeyMaps().get("main");
+				keyMap.bind(new Reference("tailtip-toggle"), KeyMap.alt("s"));
+
+				String prompt = "GTFSOSMImport> ";
+				String rightPrompt = null;
+
+				// start the shell and process input until the user quits with Ctrl-D
+				String line;
+
+				//visualizzo il menu appena avvio il tool
+				systemRegistry.execute("help");
+
+				while (true) {
+					try {
+						systemRegistry.cleanUp();
+						line = reader.readLine(prompt, rightPrompt, (MaskingCallback) null, null);
+						systemRegistry.execute(line);
+					} catch (UserInterruptException e) {
+						// Ignore
+					} catch (EndOfFileException e) {
+						return;
+					} catch (Exception e) {
+						systemRegistry.trace(e);
+					}
+				}
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		initChecks();
 		System.out.println("Welcome to GTFS-OSM-Import!\n");
 
-		int exitCode = new CommandLine(new GTFSOSMImport()).execute(args);
-		System.exit(exitCode);
+		if(args[0].equals("interactive")){
+			interactive();
+		} else {
+
+			CommandLine commandLine = new CommandLine(new GTFSOSMImport());
+
+
+			int exitCode = commandLine.execute(args);
+			System.exit(exitCode);
+		}
 	}
 	
 	private static void initChecks(){
