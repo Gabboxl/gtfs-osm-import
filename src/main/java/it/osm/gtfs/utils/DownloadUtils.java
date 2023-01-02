1a238d3dd024f27ae803c939d80322095aa9d934
Gabriele <gabboxl0@gmail.com>
2023-01-02 17:05:05 +0100

    Reformat DownloadUtils.java

--- src/main/java/it/osm/gtfs/utils/DownloadUtils.java
+++ src/main/java/it/osm/gtfs/utils/DownloadUtils.java
@@ -1,37 +1,35 @@
 /**
- Licensed under the GNU General Public License version 3
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.gnu.org/licenses/gpl-3.0.html
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-
+ * Licensed under the GNU General Public License version 3
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  **/
 package it.osm.gtfs.utils;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.*;
+import java.io.*;
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.net.URL;
 
 public class DownloadUtils {
-    private static final int TIMEOUT = 30*60000;
+    private static final int TIMEOUT = 30 * 60000;
 
-    public static void download(String url, File dest) throws IOException{
+    public static void download(String url, File dest) throws IOException {
         int retry = 0;
-        while (++retry <= 3){
+        while (++retry <= 3) {
             System.out.println("Downloading " + url + " Retry count: " + retry);
-            try{
+            try {
                 //System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
 
-                HttpURLConnection conn = (HttpURLConnection)new URL(url).openConnection();
+                HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
                 //conn.setConnectTimeout(TIMEOUT);
                 //conn.setReadTimeout(TIMEOUT);
                 conn.setRequestMethod("GET");
@@ -42,16 +40,20 @@ public class DownloadUtils {
 
                 InputStream in = conn.getInputStream();
                 FileOutputStream fos = new FileOutputStream(dest);
-                BufferedOutputStream bout = new BufferedOutputStream(fos,1024);
+                BufferedOutputStream bout = new BufferedOutputStream(fos, 1024);
                 byte[] data = new byte[1024];
-                int x=0;
-                while((x=in.read(data,0,1024))>=0){
-                    bout.write(data,0,x);
+
+                int x = 0;
+
+                while ((x = in.read(data, 0, 1024)) >= 0) {
+                    bout.write(data, 0, x);
                 }
+
                 bout.close();
                 in.close();
+                
                 return;
-            }catch(SocketTimeoutException | ConnectException e){
+            } catch (SocketTimeoutException | ConnectException e) {
                 e.printStackTrace();
             }
         }
