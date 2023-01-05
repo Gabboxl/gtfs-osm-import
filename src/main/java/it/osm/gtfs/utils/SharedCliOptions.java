0a7a7dab05cfd86586d411cda8ae67cb73e3c040
Gabriele <gabboxl0@gmail.com>
2023-01-05 19:17:32 +0100

    Added custom help output to sharedclioptions class

--- src/main/java/it/osm/gtfs/utils/SharedCliOptions.java
+++ src/main/java/it/osm/gtfs/utils/SharedCliOptions.java
@@ -2,6 +2,11 @@ package it.osm.gtfs.utils;
 
 import picocli.CommandLine;
 
+@CommandLine.Command(synopsisHeading      = "%nUsage: ",
+        descriptionHeading   = "Description: ",
+        parameterListHeading = "%nParameters:%n",
+        optionListHeading    = "%nOptions:%n",
+        commandListHeading   = "%nCommands:%n")
 public class SharedCliOptions {
     @CommandLine.Option(names = {"-c", "--checkeverything"}, description = "Check stops with the operator tag value different than what is specified in the properties file")
     Boolean checkStopsOfAnyOperatorTagValue = false;
