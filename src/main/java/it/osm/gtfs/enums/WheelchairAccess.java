0095323002d7dcbec79f8a0d2b56dec7d04e2c7f
Gabriele <gabboxl0@gmail.com>
2023-02-08 09:32:31 +0100

    Fix parsing for different GTFS datasets
    - in particular files without quotes
    - fixes parsing for data without wheelchair boarding info

--- src/main/java/it/osm/gtfs/enums/WheelchairAccess.java
+++ src/main/java/it/osm/gtfs/enums/WheelchairAccess.java
@@ -1,7 +1,7 @@
 package it.osm.gtfs.enums;
 
 public enum WheelchairAccess { //data according to the GTFS reference guide - the OSM yes value is not present in GTFS reference
-    UNKNOWN(0, "unknown"),
+    UNKNOWN(0, null),
     LIMITED(1, "limited"),
     NO(2, "no"),
 
